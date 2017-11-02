24aaf652f931951b21be314e8466baf5f6b6bbf9
manueladiazcuniche <33104725+manueladiazcuniche@users.noreply.github.com>
2017-11-01 20:52:44 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodo_inserccion
+{
+	class 
+		Program
+	{
+		static void Main(string[] args)
+		{
+			//metodo de inserccion
+
+
+            int[] numeros = new int[5] { 10, 25, 05, 01, 100 };
+			int longitud = 5;//cinco datos incluidos en el vector
+			Console.WriteLine("programa metodo de insercion");
+			int auxiliar;
+			int x;
+			for (int i = 0; i < numeros.Length; i++)
+			{
+				auxiliar = numeros[i];
+				x = i - 1;
+				while (x >= 0 && numeros[x] > auxiliar)
+				{
+					numeros[x + 1] = numeros[x];
+					x--;
+				}
+				numeros[x + 1] = auxiliar;
+			}
+			for (int i = 0; i < longitud; i++)
+				Console.WriteLine(" " + numeros[i]);
+			Console.Read();
+
+		}
+	}
+}
+
+
+
+
+
+
---
+++ Program1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodo_burbuja
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int n = 0, m = 0;
+			int[] numeros;
+
+			Console.WriteLine("escriba la cantidad de numeros");
+			n = int.Parse(Console.ReadLine());
+			numeros = new int[n];
+			for (int a = 0; a < n; a++)
+			{
+				Console.Write("escriba el numero [{0}]:", a);
+				numeros[a] = int.Parse(Console.ReadLine());
+			}
+			for (int b = 1; b < n; b++)
+			{
+				for (int c = 0; c < n - 1; c++)
+				{
+					if (numeros[b] < numeros[c])
+					{
+						m = numeros[b];
+						numeros[b] = numeros[c];
+						numeros[c] = m;
+					}
+				}
+			}
+			for (int r = 0; r < n; r++)
+				Console.WriteLine("{0}.-{1}", r + 1, numeros[r]);
+			Console.Read();
+		}
+	}
+}
+
